7b3774ce69f8daa4b47994a4e6124bd4a06143cd
kotres <lucaskotres@gmail.com>
2016-07-13 15:52:21 -0300

    New tools

--- Tools/removeoutliers2.py
+++ Tools/removeoutliers2.py
@@ -4,8 +4,8 @@ import numpy as np #biblioteca numpy - http://www.numpy.org/
 a = np.array([25, 26, 225, 24, 23, 24, 25, 325, 28, 27]) #array com os elementos do conjunto
 
 
-def removeoutlier(values):                     #define a função com uma variavel de entrada
-    fator = 1.5                                #1.5 é o fator de multiplicacao
+def removeoutlier(values):                     #define a funcao com uma variavel de entrada
+    fator = 1.5                                #1.5 e o fator de multiplicacao
     q75, q25 = np.percentile(values, [75, 25]) #retorna o terceiro e primeiro quartil
     iqr = q75 - q25                            #calcula o iqr(interquartile range)
 
